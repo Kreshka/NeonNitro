c098c6de8bef2196ccde0e34fc1b50194c672552
Kreshka <kre2011@bk.ru>
2025-02-03 11:15:32 +0300

    adding numbers updating code starting creating skins menu

--- main.py
+++ main.py
@@ -16,19 +16,28 @@ pygame.mixer.init()
 screen = pygame.display.set_mode((0, 0), 1)
 pygame.display.set_caption("NeonNitro")
 clock = pygame.time.Clock()
-menu = pygame.transform.scale(pygame.image.load('date/menu_screen.png'), (W, H))
+menu = pygame.transform.scale(pygame.image.load('data/menu_screen.png'), (W, H))
 game = pygame.Surface((W, H))
-settings = pygame.transform.scale(pygame.image.load('date/settings_screen.png'), (W, H))
+settings = pygame.transform.scale(pygame.image.load('data/settings_screen.png'), (W, H))
+game_over = pygame.transform.scale(pygame.image.load('data/game_over_screen.png'), (W, H))
+n1, n2, n3, go = (
+    pygame.transform.scale(pygame.image.load('data/number1.png'), (W, H)),
+    pygame.transform.scale(pygame.image.load('data/number2.png'), (W, H)),
+    pygame.transform.scale(pygame.image.load('data/number3.png'), (W, H)),
+    pygame.transform.scale(pygame.image.load('data/GO!_screen.png'), (W, H)),
+)
 now_screen = menu
 all_sprites = pygame.sprite.Group()
+game_over_group = pygame.sprite.Group()
+start_nums_group = pygame.sprite.Group()
 cars = pygame.sprite.Group()
 xs = []
 
 
 class Car(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__(cars)
-        self.image = pygame.transform.scale(pygame.image.load('date/main_car.png'), (133, 245))
+    def __init__(self, cl):
+        super().__init__(cl)
+        self.image = pygame.transform.scale(pygame.image.load('data/main_car.png'), (133, 245))
         self.rect = self.image.get_rect()
         self.rect.x = 500
         self.rect.y = 740
@@ -36,22 +45,32 @@ class Car(pygame.sprite.Sprite):
 
 class MainCar(Car):
     def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load('date/main_car.png')
+        super().__init__(all_sprites)
+        self.image = pygame.image.load('data/main_car.png')
         self.s = 25
 
     def update(self, *args, **kwargs):
-        g = pygame.key.get_pressed()
-        if g[pygame.K_d] or g[pygame.K_RIGHT]:
-            self.rect.x += self.s
-        elif g[pygame.K_a] or g[pygame.K_LEFT]:
-            self.rect.x -= self.s
+        global now_screen, game_running, game_over_flag
+        if not game_over_flag:
+            g = pygame.key.get_pressed()
+            if g[pygame.K_d] or g[pygame.K_RIGHT]:
+                self.rect.x += self.s
+            elif g[pygame.K_a] or g[pygame.K_LEFT]:
+                self.rect.x -= self.s
+            if pygame.sprite.spritecollide(self, cars, dokill=1):
+                game_over_flag = True
+            if self.rect.x <= 180:
+                self.rect.x += self.s
+            if self.rect.x >= 1750 - self.rect.w:
+                self.rect.x -= self.s
+        else:
+            pass
 
 
 class WCar(Car):
     def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load('date/d_car1.png')
+        super().__init__(cars)
+        self.image = pygame.image.load('data/d_car1.png')
         self.s = random.randint(40, 50)
         self.rect.x = random.randint(280, 1600)
         self.rect.y = random.randint(-7000, -2000)
@@ -61,17 +80,20 @@ class WCar(Car):
         xs.append(self.rect.x)
 
     def update(self, *args, **kwargs):
-        self.rect.y += self.s
-        if len(pygame.sprite.spritecollide(self, cars, dokill=0)) > 1:
-            self.kill()
-            dcars.append(WCar())
-        if self.rect.y >= 1080:
-            self.rect.y = -1000
-            self.rect.x = random.randint(280, 1600)
+        if not game_over_flag:
+            self.rect.y += self.s
+            if len(pygame.sprite.spritecollide(self, cars, dokill=0)) > 1:
+                self.kill()
+                dcars.append(WCar())
+            if self.rect.y >= 1080:
+                self.rect.y = -1000
+                self.rect.x = random.randint(280, 1600)
+        else:
+            pass
 
 
 class Field(pygame.sprite.Sprite):
-    image = pygame.transform.scale(pygame.image.load('date/road.png'), (W, H))
+    image = pygame.transform.scale(pygame.image.load('data/road.png'), (W, H))
 
     def __init__(self, p):
         super().__init__(all_sprites)
@@ -84,55 +106,138 @@ class Field(pygame.sprite.Sprite):
             self.rect.y = -1080
 
     def update(self, *args, **kwargs):
-        self.rect.y += 18
-        if self.rect.y >= 1080:
-            self.rect.y = -1080
+        if not game_over_flag:
+            self.rect.y += 18
+            if self.rect.y >= 1080:
+                self.rect.y = -1080
+
+
+class GameOver(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(game_over_group)
+        self.image = game_over
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = -1920
+        self.s = 10
+
+    def update(self, *args, **kwargs):
+        if game_over_flag:
+            if self.rect.y >= -self.s:
+                self.rect.y = 0
+                self.s = 0
+            else:
+                self.rect.y += self.s
+                self.s += 10
+
+
+class StartingGameNums(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(start_nums_group)
+        self.images = [n3, n2, n1, go]
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
+        self.f = 1
+
+    def update(self, *args, **kwargs):
+        global starting_flag
+        if self.f == 15:
+            self.image = self.images[1]
+        if self.f == 30:
+            self.image = self.images[2]
+        if self.f == 45:
+            self.image = self.images[3]
+        if self.f == 60:
+            starting_flag = True
+        self.f += 1
 
 
 p = 0
 p2 = 0
 running = True
+game_running = True
+sets_running = True
+game_over_flag = False
+starting_flag = False
+dcars = []
+
+
+def game_loop():
+    global now_screen, game_running, game_over_flag, starting_flag
+    game_running = True
+    game_over_flag = False
+    starting_flag = False
+    start_nums_obj = StartingGameNums()
+    while not starting_flag:
+        screen.fill((0, 0, 0))
+        game.fill((0, 0, 0))
+        start_nums_group.update()
+        start_nums_group.draw(game)
+        screen.blit(game, (0, 0))
+        pygame.display.flip()
+    field = Field(1)
+    field2 = Field(0)
+    car = MainCar()
+    game_over_obj = GameOver()
+    dcars = [WCar() for _ in range(5)]
+    while game_running:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE and game_over_flag:
+                    game_running = False
+
+        screen.fill((0, 0, 0))
+        game.fill((0, 0, 0))
+        all_sprites.update()
+        all_sprites.draw(game)
+        cars.update()
+        cars.draw(game)
+        game_over_group.update()
+        game_over_group.draw(game)
+        screen.blit(game, (0, 0))
+        pygame.display.flip()
+        clock.tick(60)
+    field.kill()
+    field2.kill()
+    car.kill()
+    game_over_obj.kill()
+    start_nums_obj.kill()
+    dcars = list(map(lambda x: x.kill(), dcars))
+
+
+def skins_menu():
+    global sets_running
+    sets_running = True
+    while sets_running:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    sets_running = False
+
+        screen.fill((0, 0, 0))
+        screen.blit(settings, (0, 0))
+        pygame.display.flip()
+
+
 while running:
-    if now_screen == game and p == 0:
-        field = Field(1)
-        field2 = Field(0)
-        car = MainCar()
-        dcars = [WCar() for i in range(5)]
-        p = 1
-        p2 = 1
-    if now_screen == menu and p2 == 1:
-        field.kill()
-        field2.kill()
-        car.kill()
-        dcars = list(map(lambda x: x.kill(), dcars))
-        p2 = 0
-        p = 0
-    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if now_screen == menu:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if 830 < event.pos[0] < 1090 and 540 < event.pos[1] < 610 and event.button == 1:
-                    now_screen = game
-                if 410 < event.pos[0] < 610 and 790 < event.pos[1] < 860 and event.button == 1:
-                    running = False
-                if 1340 < event.pos[0] < 1520 and 190 < event.pos[1] < 370 and event.button == 1:
-                    now_screen = settings
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    running = False
-        else:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    now_screen = menu
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if 830 < event.pos[0] < 1090 and 540 < event.pos[1] < 610 and event.button == 1:
+                game_loop()
+            if 410 < event.pos[0] < 610 and 790 < event.pos[1] < 860 and event.button == 1:
+                running = False
+            if 1310 <= event.pos[0] <= 1590 and 270 <= event.pos[1] <= 340 and event.button == 1:
+                skins_menu()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
     screen.fill((0, 0, 0))
-    game.fill((0, 0, 0))
-    all_sprites.update()
-    all_sprites.draw(game)
-    cars.update()
-    cars.draw(game)
-    screen.blit(now_screen, (0, 0))
+    screen.blit(menu, (0, 0))
     pygame.display.flip()
 
 pygame.quit()
